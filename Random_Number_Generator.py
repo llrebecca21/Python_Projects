155f51ee7cf0f23081b9353268097d99ad6df9d4
Rebecca Lee <89818084+llrebecca21@users.noreply.github.com>
2022-04-11 21:57:00 -0500

    Create psuedocode for the program

---
+++ Random_Number_Generator.py
@@ -0,0 +1,11 @@
+'''
+CIS 122 Winter 2020 Lab 08
+Author: Rebecca Lee
+Credit:
+Description: Create a random number generator
+"Work in Progress"
+'''
+
+# Import random
+
+# Create gen_random_integer_list function
