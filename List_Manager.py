7f4da0fb2d446bedc9dc3ab3a183650ec210376e
Rebecca Lee <89818084+llrebecca21@users.noreply.github.com>
2022-04-11 20:37:56 -0500

    Create pseudocode for the program

--- List_Manager.py
+++ List_Manager.py
@@ -2,5 +2,46 @@
 CIS 122 Winter 2020 Assignment 5
 Author: Rebecca Lee
 Credit:
-Description: Create a program such that given a number between 1 and 365 or 366 inclusive and a year, it will give what date it is
+Description: Create a basic list manager program
 '''
+
+# Create a list variable
+
+t = []
+
+# All the functions that are called
+
+
+
+# Create pad_string function
+
+# Create pad_left function
+
+# Create pad_right function
+
+# Create a command list
+
+# Create cmd_help function
+
+# Create a cmd_add function
+
+# Create a cmd_delete function
+
+# Create a cmd_list function
+
+# Create a cmd_clear function
+
+# Create a get_max_list_item_size function
+
+# Create a program that will iterate through the program
+
+
+
+
+
+
+
+
+
+
+
