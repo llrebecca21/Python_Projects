7cc1f249da163c53d30538e6c1a45487e2983cec
Rebecca Lee <89818084+llrebecca21@users.noreply.github.com>
2022-04-11 20:33:14 -0500

    Delete and Rename the Project File for List Manager

--- List Manager.py
+++
@@ -1,8 +0,0 @@
-'''
-CIS 122 Winter 2020 Assignment 5
-Author: Rebecca Lee
-Credit:
-Description: Create a basic list manager program
-'''
-
-
---
+++ List_Manager.py
@@ -0,0 +1,6 @@
+'''
+CIS 122 Winter 2020 Assignment 5
+Author: Rebecca Lee
+Credit:
+Description: Create a program such that given a number between 1 and 365 or 366 inclusive and a year, it will give what date it is
+'''
